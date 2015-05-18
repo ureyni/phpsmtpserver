819c1ac6c0840dd8bf9a3db40245986c9a75bab8
ureyni <hasan.ucak@gmail.com>
2015-05-18 14:55:16 +0000

    simple smtp with pthreads

--- regexaddr.php
+++ regexaddr.php
@@ -8,10 +8,10 @@
  *   (?(?=[^a-z]*[a-z])
   \d{2}-[a-z]{3}-\d{2}  |  \d{2}-\d{2}-\d{2} )
  */
-$str = '"hasan ucak" hasan.ucak@gmail.com';
-if (preg_match_all('/(?(?=^\")(^\"(.*)\")(\s|)\<([\._a-z0-9-]+)@(gmail.com|eimza.gmail.com|yahoo.com|hotmail.com)\>$|'
+$str = '"hasan ucak" <hasan.ucak@gmail.com>';
+if (preg_match_all('/(?(?=^\")(^\"(.*)\")([\s]+|)\<([\._a-z0-9-]+)@(gmail.com|eimza.gmail.com|yahoo.com|hotmail.com)\>$|'
                 . '(^[\._a-z0-9-]+)@(gmail.com|eimza.gmail.com|yahoo.com|hotmail.com)$)/', $str, $matches, PREG_SET_ORDER)||
-        preg_match_all('/(?(?=^\")(^\"(.*)\")(\s|)\<([\._a-z0-9-]+)@([_a-z0-9-]+)\.(gmail.com|eimza.gmail.com|yahoo.com|hotmail.com)\>$|'
+        preg_match_all('/(?(?=^\")(^\"(.*)\")([\s]+|)\<([\._a-z0-9-]+)@([_a-z0-9-]+)\.(gmail.com|eimza.gmail.com|yahoo.com|hotmail.com)\>$|'
                 . '(^[\._a-z0-9-]+)@([_a-z0-9-]+)\.(gmail.com|eimza.gmail.com|yahoo.com|hotmail.com)$)/', $str, $matches, PREG_SET_ORDER)
         )
     print "valid";
