ddc365c3b033768ad821f1ec2be52eacb4026590
Hasan UCAK <hasan.ucak@gmail.com>
2015-08-28 18:38:52 +0300

    allways run run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 PID=`pgrep -lf phpsmtpserver|cut -d " " -f1`
 kill -9 $PID
-/opt/php56/bin/php /opt/smtp/phpsmtpserver.php >> /opt/smtp/phpsmtpserver.log 2>&1 &
+while true;do
+/opt/php56/bin/php /opt/smtp/phpsmtpserver.php >> /opt/smtp/phpsmtpserver.log 2>&1
+done
