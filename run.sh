a1bbf01a6d84cad5699ef28a6462abc351bdbe10
Hasan UCAK <hasan.ucak@gmail.com>
2015-09-11 16:08:35 +0300

    zend_mm_heap corrupted error

--- run.sh
+++ run.sh
@@ -2,5 +2,8 @@ PID=`pgrep -lf phpsmtpserver|cut -d " " -f1`
 kill -9 $PID
 while true; 
 do
+          #if have recently  zend_mm_heap corrupted error ..this is set
+          #php will not use zend memory management
+           export USE_ZEND_ALLOC=0
           /opt/php/bin/php /opt/smtp/phpsmtpserver.php >> /opt/smtp/phpsmtpserver.log 2>&1
 done
